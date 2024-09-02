a7743e9f4f156ecd9d2d45ddec97e05dac6c9fd9
Melvin Redondo--Tanis <melvin@redondotanis.com>
2024-09-02 11:27:28 +0200

    chore: Add initial files for chatbot introduction and debugging exercises

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+def factorial(n):
+    """
+    Calculate the factorial of a given number.
+
+    Parameters:
+    - n (int): The number for which the factorial is to be calculated.
+
+    Returns:
+    - int: The factorial of the given number.
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
